03eb387362778d76dbd6107161b24507dfcd2664
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-11 14:24:21 +0400

    feat: Added getUserNames function

---
+++ js/useMapMethod.js
@@ -0,0 +1,22 @@
+// Your function must use map method
+
+// Write a function which returns array of usernames.
+
+const users = [
+  {
+    username: "Yuri Gagarin",
+    lang: "ru",
+  },
+  {
+    username: "Nil Armstrong",
+    lang: "ENG",
+  },
+];
+
+function getUserNames(arr) {
+  let tempArr = arr;
+  tempArr = tempArr.map((item) => item.username);
+  return tempArr;
+}
+
+console.log(getUserNames(users)); // ['Yuri Gagarin', 'Nil Armstrong']
