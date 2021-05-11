fb712794c08f2754d4911285a40b1929d7ccdf83
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-11 17:10:24 +0400

    feat: Added myFind function

---
+++ js/myFind.js
@@ -0,0 +1,21 @@
+let arrOfObjs = [
+  { name: "Samvel", age: 40 },
+  { name: "Samvel", age: 14 },
+  { name: "Samvel", age: 10 },
+  { name: "Samvel", age: 20 },
+  { name: "Samvel", age: 23 },
+  { name: "Samvel", age: 25 },
+  { name: "Samvel", age: 26 },
+];
+
+function myFindIndex(array, age) {
+  for (let i = 0; i < array.length; i += 1) {
+    let element = array[i];
+    if (element.age === age) {
+      return element;
+    }
+  }
+  return -1;
+}
+
+console.log(myFindIndex(arrOfObjs, 20));
