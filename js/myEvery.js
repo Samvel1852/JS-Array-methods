7d6290b9d60926408e0c1ff7addd660fb51bdc31
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-11 17:29:47 +0400

    feat: Added myEvery function

---
+++ js/myEvery.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function myEvery(array, checkFn) {
+  let resArr = [];
+  let trueCount = 0;
+  array.forEach((element) => {
+    if (checkFn(element)) {
+      trueCount += 1;
+    }
+  });
+  if (trueCount === array.length) {
+    return true;
+  }
+  return false;
+}
+
+function checkFn(item) {
+  if (item > 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(myEvery(arr, checkFn));
